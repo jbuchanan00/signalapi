0c64afbcfda26507f15cf478120865b6ea044a53
Josh <josh.buchanan00@gmail.com>
2026-01-04 18:41:49 -0700

    Changed java version to 24 for my maven configuration. Able to communicate with halourl as well. Dockerfile needs work.

---
+++ src/main/java/com/inkedout/Signal/services/WebClientInstance.java
@@ -0,0 +1,29 @@
+package com.inkedout.Signal.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class WebClientInstance {
+    private static final Logger log = LoggerFactory.getLogger(WebClientInstance.class);
+    private final WebClient client;
+
+    public WebClientInstance(String baseAddress){
+        this.client = WebClient.create(baseAddress);
+        log.info("Client base is: {}", baseAddress);
+    }
+
+    public Mono<String> getData(String uri) {
+
+        return client.get()
+                .uri(uri)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, response ->
+                        Mono.error(new RuntimeException("Client Error: " + response.statusCode())))
+                .onStatus(HttpStatusCode::is5xxServerError, response ->
+                        Mono.error(new RuntimeException("Server Error: " + response.statusCode())))
+                .bodyToMono(String.class);
+    }
+}
